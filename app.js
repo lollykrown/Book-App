d1412d3771f93af2b683b8e16bba09aeb55735de
Kay <joe_kayu@yahoo.com>
2020-08-14 22:05:17 +0100

    .

--- app.js
+++ app.js
@@ -8,36 +8,44 @@ const connection = new Sequelize('db', 'user', 'pass', {
   host: 'localhost',
   dialect: 'sqlite',
   storage: 'db.sqlite',
-  operatorsAliases: false 
-})
-
-const User = connection.define('User', {
-  uuid: {
-    type: Sequelize.UUID,
-    primaryKey: true,
-    defaultValue: Sequelize.UUIDV4
-  },
-  name: Sequelize.STRING,
-  bio: Sequelize.TEXT
 })
 
 connection
-  .sync({
-    logging: console.log,
-    force: true
-  })
+  .authenticate()
   .then(() => {
-    User.create({
-      name: 'Joe',
-      bio: 'New bio entry 2'
-    })
-  })
-  .then(() => {
-    console.log('Connection to database established successfully.');
+    console.log('Connection established')
   })
   .catch(err => {
     console.error('Unable to connect to the database:', err);
 });
+
+// const User = connection.define('User', {
+//   uuid: {
+//     type: Sequelize.UUID,
+//     primaryKey: true,
+//     defaultValue: Sequelize.UUIDV4
+//   },
+//   name: Sequelize.STRING,
+//   bio: Sequelize.TEXT
+// })
+
+// connection
+//   .sync({
+//     logging: console.log,
+//     force: true
+//   })
+//   .then(() => {
+//     User.create({
+//       name: 'Joe',
+//       bio: 'New bio entry 2'
+//     })
+//   })
+//   .then(() => {
+//     console.log('Connection to database established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+// });
 
 
 app.listen(port, () => {
