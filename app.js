fbda19cd4f6c5e7ff17fb165d6f1dbe2926217e9
Kay <joe_kayu@yahoo.com>
2020-08-14 22:28:20 +0100

    .

--- app.js
+++ app.js
@@ -11,14 +11,10 @@ const connection = new Sequelize('db', 'user', 'pass', {
 })
 
 const User = connection.define('User', {
-  // uuid: {
-  //   type: Sequelize.UUID,
-  //   primaryKey: true,
-  //   defaultValue: Sequelize.UUIDV4
-  // },
-    uid: {
-    type: Sequelize.INTEGER,
+  uuid: {
+    type: Sequelize.UUID,
     primaryKey: true,
+    defaultValue: Sequelize.UUIDV4
   },
   name: Sequelize.STRING,
   bio: Sequelize.TEXT
